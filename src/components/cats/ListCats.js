17802ae819a52892f7712d600dc8a0e03a73cad9
Atef Haque <atef.haque@insidemaps.com>
2021-03-26 01:05:22 +0000

    make app responsive

--- src/components/cats/ListCats.js
+++ src/components/cats/ListCats.js
@@ -34,14 +34,21 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     color: "rgba(255, 255, 255, 0.54)",
   },
+  imgGrid: {
+    backdropFilter: "sepia(0.5)",
+    paddingBottom: "0px !important",
+  },
   img: {
     height: theme.breakpoints.width > 340 ? "auto" : 240,
     width: theme.breakpoints.width > 340 ? 340 : "auto",
   },
+  actionGrid: {
+    backgroundColor: theme.palette.secondary.light,
+  },
   votes: {
-    width: 220,
     display: "inline-flex",
-    marginLeft: 14,
+    marginTop: 14,
+    marginLeft: 20,
   },
 }));
 
@@ -59,56 +66,64 @@ export default function ListCats(props) {
   }, [loadedCats]);
 
   return (
-    <Container maxWidth="xl">
-      <Grid container spacing={3} className={classes.root}>
-        {cats.map(({ id, url, favouriteId, votes }) => {
-          return (
-            <Grid item xl={3} lg={4} md={6} xs={12} key={id}>
-              <Paper elevation={2} className={classes.paper}>
-                <Grid container spacing={2} direction="column">
-                  <Grid item xs={12}>
-                    <img src={url} alt={id} className={classes.img} />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="button" className={classes.votes}>
-                      Votes: {votes}
-                    </Typography>
-                    <IconButton
-                      onClick={() => {
-                        if (favouriteId) {
-                          dispatch(unfavourite({ favouriteId }));
-                        } else {
-                          dispatch(favourite({ imageId: id }));
-                        }
-                      }}
-                    >
-                      {favouriteId !== undefined ? (
-                        <FavoriteOutlined />
-                      ) : (
-                        <FavoriteBorderOutlined />
-                      )}
-                    </IconButton>
-                    <IconButton
-                      onClick={() => {
-                        dispatch(vote({ imageId: id, value: 1 }));
-                      }}
-                    >
-                      <ArrowUpwardOutlined />
-                    </IconButton>
-                    <IconButton
-                      onClick={() => {
-                        dispatch(vote({ imageId: id, value: 0 }));
-                      }}
-                    >
-                      <ArrowDownwardOutlined />
-                    </IconButton>
+    <Grid container spacing={3} className={classes.root}>
+      {cats.map(({ id, url, favouriteId, votes }) => {
+        return (
+          <Grid item xl={3} lg={4} md={6} xs={12} key={id}>
+            <Paper elevation={2} className={classes.paper}>
+              <Grid container spacing={2} direction="column">
+                <Grid item xs={12} className={classes.imgGrid}>
+                  <img src={url} alt={id} className={classes.img} />
+                </Grid>
+                <Grid item xs={12} className={classes.actionGrid}>
+                  <Grid container spacing={1}>
+                    <Grid item xs={6}>
+                      <Typography variant="button" className={classes.votes}>
+                        Votes: {votes}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <IconButton
+                        onClick={() => {
+                          if (favouriteId) {
+                            dispatch(unfavourite({ favouriteId }));
+                          } else {
+                            dispatch(favourite({ imageId: id }));
+                          }
+                        }}
+                      >
+                        {favouriteId !== undefined ? (
+                          <FavoriteOutlined />
+                        ) : (
+                          <FavoriteBorderOutlined />
+                        )}
+                      </IconButton>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <IconButton
+                        onClick={() => {
+                          dispatch(vote({ imageId: id, value: 1 }));
+                        }}
+                      >
+                        <ArrowUpwardOutlined />
+                      </IconButton>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <IconButton
+                        onClick={() => {
+                          dispatch(vote({ imageId: id, value: 0 }));
+                        }}
+                      >
+                        <ArrowDownwardOutlined />
+                      </IconButton>
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Paper>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Container>
+              </Grid>
+            </Paper>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
